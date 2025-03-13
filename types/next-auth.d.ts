90927e45986ecb47810e0e9d703d659bab5d811b
Otavio Serra <otavio.serra@contractors.roche.com>
2025-03-13 22:34:32 +0000

    fixes

---
+++ types/next-auth.d.ts
@@ -0,0 +1,16 @@
+import "next-auth";
+
+declare module "next-auth" {
+    interface User {
+        id: string;
+        name?: string | null;
+        email?: string | null;
+        image?: string | null;
+    }
+
+    interface Session {
+        user: User & {
+            id: string;
+        };
+    }
+} 
